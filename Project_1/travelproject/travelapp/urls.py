c0d448686152c41fb00d1c66c889909adddd0180
Arjun Jayesh <9997arjun@gmail.com>
2022-08-20 12:53:50 +0530

    Setting up static website and linking contents

--- Project_1/travelproject/travelapp/urls.py
+++ Project_1/travelproject/travelapp/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('',views.demo, name='demo'),
     path('about/',views.about,name='about'),
-    path('add/',views.add,name='add'),
 ]
--- Project_1/travelproject/travelapp/views.py
+++ Project_1/travelproject/travelapp/views.py
@@ -3,15 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def demo(request):
-    name="India"
-    return render(request,"index.html",{'obj':name})
+    return render(request,"index.html")
 
 def about(request):
     return render(request,'about.html')
-
-
-def add(request):
-    x = int(request.GET['num1'])
-    y = int(request.GET['num2'])
-    res=x+y
-    return render(request,"result.html",{'result':res})
