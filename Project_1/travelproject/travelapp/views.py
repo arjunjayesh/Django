7c8b9876b662b57c839638925ef942cfdcd70b56
Arjun Jayesh <9997arjun@gmail.com>
2022-08-20 11:23:15 +0530

    Passing Value to HTML using dictionary and jinja format

--- Project_1/travelproject/travelapp/views.py
+++ Project_1/travelproject/travelapp/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def demo(request):
-    return render(request,"index.html")
+    name="India"
+    return render(request,"index.html",{'obj':name})
 
 def about(request):
     return render(request,'about.html')
