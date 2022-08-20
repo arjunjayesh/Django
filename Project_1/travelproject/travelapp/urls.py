aa2f647ae135b013aebcdd40d40bd63c338f2b7b
Arjun Jayesh <9997arjun@gmail.com>
2022-08-20 11:15:45 +0530

    Multiple Views - Navigating to other page

--- Project_1/travelproject/travelapp/urls.py
+++ Project_1/travelproject/travelapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.demo, name='demo'),
+    path('about/',views.about,name='about'),
 ]
--- Project_1/travelproject/travelapp/views.py
+++ Project_1/travelproject/travelapp/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def demo(request):
-    return HttpResponse("HelloWorld")
+    return render(request,"index.html")
+
+def about(request):
+    return render(request,'about.html')
