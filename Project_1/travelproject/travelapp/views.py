30eb648130e78498c58220a78cd2d727bca6ffd4
Arjun Jayesh <9997arjun@gmail.com>
2022-08-20 11:37:37 +0530

    Passing Value from one Page to another

--- Project_1/travelproject/travelapp/views.py
+++ Project_1/travelproject/travelapp/views.py
@@ -8,3 +8,10 @@ def demo(request):
 
 def about(request):
     return render(request,'about.html')
+
+
+def add(request):
+    x = int(request.GET['num1'])
+    y = int(request.GET['num2'])
+    res=x+y
+    return render(request,"result.html",{'result':res})
