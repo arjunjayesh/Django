676576c8ad4b0afc59dfc6940fe3f766e780506b
Arjun Jayesh <9997arjun@gmail.com>
2022-08-23 13:00:54 +0530

    Create new app 'credentials'

--- Project_1/travelproject/travelproject/urls.py
+++ Project_1/travelproject/travelproject/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('travelapp.urls'))
+    path('',include('travelapp.urls')),
+    path('credentials/',include('credentials.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
