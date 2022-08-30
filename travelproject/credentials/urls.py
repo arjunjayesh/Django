960e4674ef5af60cc1126e77e2c7517cc5cc827f
Arjun Jayesh <9997arjun@gmail.com>
2022-08-30 12:37:30 +0530

    Changes

--- travelproject/credentials/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path, include
-app_name='credentials'
-
-urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-]
--- travelproject/travelapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('',views.demo, name='demo'),
-    path('about/',views.about,name='about'),
-]
